f737c6ceb38d590da000b4f7b76d0fafde4442e4
Siwa_Nick <t_tinfinity@hotmail.com>
2017-09-12 13:41:14 +0700

    edit variable

--- app/js/dialog.js
+++ app/js/dialog.js
@@ -1,10 +1,15 @@
 $(function () {
     $("#dialog-message").dialog({
         modal: true,
+        autoOpen: false,
         buttons: {
             Ok: function () {
                 $(this).dialog("close");
             }
         }
     });
+
+    $( "#upload" ).on( "click", function() {
+        $( "#dialog-message" ).dialog( "open" );
+      });
 });
